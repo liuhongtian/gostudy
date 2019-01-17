c655878c3d9b98746ced7da21dd79f9f7d0482a3
刘洪天 <liuht@bhz.com.cn>
2019-01-17 10:36:58 +0800

    a project to study golang

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "fmt"
+  "github.com/liuhongtian/mygolibs/stringutil"
+)
+
+func main() {
+  fmt.Println("Hello, GO!")
+  fmt.Println(stringutil.Revers("Hello, GO!"))
+}
+
