e1af1eeefed21487aadf6c752fe4655e1de9eff9
刘洪天 <liuht@bhz.com.cn>
2019-01-17 11:11:50 +0800

    add redis

--- hello.go
+++ hello.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/liuhongtian/golibs/redisutil"
 	"github.com/liuhongtian/golibs/stringutil"
 )
 
 func main() {
 	fmt.Println("Hello, GO!")
 	fmt.Println(stringutil.Revers("Hello, GO!"))
+
+	fmt.Println("redis:")
+
+	value := redisutil.GetFromRedis("liu")
+	fmt.Println(value)
+
 }
