84fa40061651343b8712e9555452f7d41c5f8c4c
刘洪天 <liuht@bhz.com.cn>
2019-01-17 11:02:13 +0800

    modified package

--- hello.go
+++ hello.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-  "fmt"
-  "github.com/liuhongtian/mygolibs/stringutil"
+	"fmt"
+	"github.com/liuhongtian/golibs/stringutil"
 )
 
 func main() {
-  fmt.Println("Hello, GO!")
-  fmt.Println(stringutil.Revers("Hello, GO!"))
+	fmt.Println("Hello, GO!")
+	fmt.Println(stringutil.Revers("Hello, GO!"))
 }
-
